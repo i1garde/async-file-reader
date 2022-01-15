629f88558015dbdde4ff361916abc7edc3814683
Ivan Komarenko <vanyakomarenko@gmail.com>
2022-01-15 13:23:27 +0200

    Second implementation of callback async file reader function

--- reading-file-with-callbacks.js
+++ reading-file-with-callbacks.js
@@ -6,10 +6,16 @@ const readFiles = (path) => {
             console.log("Can't read file!!!")
             return
         }
-        console.log(result.toString())
+        console.log("Callback inside function definition: " + result.toString())
     })
 }
 
 readFiles("text.txt")
 
 console.log("Text after async function definition.\n(so it will called first as sunc operation)\n")
+
+const readFiles2 = (path) => (callback) => fs.readFile(path, callback)
+
+readFiles2("text.txt")((err, res) =>
+    err ?
+        console.log("Can't read file!!!") : console.log("Callback outside function definition: " + res.toString()))
